bd1c98bd863345e090e9be42efe2f1585d883a83
devHaitham481 <devhaitham481@gmail.com>
2024-06-15 11:52:01 +0200

    commit nvim & tmux conf

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.winbar = "%=%m %f"
+vim.opt.scrolloff = 20
+vim.opt.scroll=10
+vim.opt.updatetime = 50
+vim.opt.tabstop = 2
+vim.opt.timeoutlen = 10
+vim.g.minimap_auto_start = 1
+vim.g.minimap_auto_start_win_enter = 1
+
